0ef4bcc4ca8ffdca5ff9029a2e3a9658a87cdd34
Philip <11591180+7068696c6970@users.noreply.github.com>
2022-05-01 02:15:56 +0200

    added frontend

---
+++ travel-planner/src/layouts/MainLayout.vue
@@ -0,0 +1,192 @@
+<template>
+  <q-layout view="hhh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white q-mb-xl">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Travel Planner
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+<!--    <q-page-container>-->
+<!--      <router-view />-->
+<!--    </q-page-container>-->
+    <q-page class="flex flex-center">
+      <q-card class="col-8 q-mt-xl">
+        <q-card-section class="col-12 full-width">
+          <h1>Travel Planner</h1>
+          <q-form
+            action="https://webhook.site/5f87d83b-b7d1-4b35-ab61-5c6c1ade027e"
+            method="post"
+            @submit.prevent="onSubmit"
+          >
+            <q-card class="q-pa-lg q-mb-md">
+              <p>Overall Safety</p>
+              <q-field outlined name="safety">
+                <q-slider
+                  name="safety"
+                  v-model="safety"
+                  marker-labels
+                  :min="0"
+                  :max="10"
+                />
+              </q-field>
+            </q-card>
+
+            <q-card class="q-pa-lg q-mb-md">
+              <p>Low Prices</p>
+              <q-field outlined name="price">
+                <q-slider
+                  name="price"
+                  v-model="price"
+                  marker-labels
+                  :min="0"
+                  :max="10"
+                />
+              </q-field>
+            </q-card>
+
+            <q-card class="q-pa-lg q-mb-md">
+              <p>Good Weather</p>
+              <q-field outlined name="weather">
+                <q-slider
+                  name="weather"
+                  v-model="weather"
+                  marker-labels
+                  :min="0"
+                  :max="10"
+                />
+              </q-field>
+            </q-card>
+
+            <q-card class="q-pa-lg q-mb-md">
+              <p>Knowledge of English</p>
+              <q-field outlined name="english">
+                <q-slider
+                  name="english"
+                  v-model="english"
+                  marker-labels
+                  :min="0"
+                  :max="10"
+                />
+              </q-field>
+            </q-card>
+
+            <q-card class="q-pa-lg q-mb-md">
+              <p>Walkability</p>
+              <q-field outlined name="walkability">
+                <q-slider
+                  name="walkability"
+                  v-model="walkability"
+                  marker-labels
+                  :min="0"
+                  :max="10"
+                />
+              </q-field>
+            </q-card>
+
+            <q-card class="q-pa-lg q-mb-md">
+              <p>Cleanliness</p>
+              <q-field outlined name="cleanliness">
+                <q-slider
+                  name="cleanliness"
+                  v-model="cleanliness"
+                  marker-labels
+                  :min="0"
+                  :max="10"
+                />
+              </q-field>
+            </q-card>
+
+            <q-card class="q-pa-lg q-mb-md">
+              <p>Religiousness</p>
+              <q-field outlined name="religiousness">
+                <q-slider
+                  name="religiousness"
+                  v-model="religiousness"
+                  marker-labels
+                  :min="0"
+                  :max="10"
+                />
+              </q-field>
+            </q-card>
+
+            <q-card class="q-pa-lg q-mb-md">
+              <p>Alcohol Culture</p>
+              <q-field outlined name="alcohol">
+                <q-slider
+                  name="alcohol"
+                  v-model="alcohol"
+                  marker-labels
+                  :min="0"
+                  :max="10"
+                />
+              </q-field>
+            </q-card>
+
+            <q-btn label="Submit" type="submit" color="primary"/>
+
+
+          </q-form>
+        </q-card-section>
+
+        <q-card-section class="row justify-center">
+
+        </q-card-section>
+      </q-card>
+    </q-page>
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import axios from 'axios'
+
+export default defineComponent({
+  name: 'MainLayout',
+  setup: () => {
+
+    const safety = ref(5)
+    const price = ref(5)
+    const weather = ref(5)
+    const english = ref(5)
+    const walkability = ref(5)
+    const cleanliness = ref(5)
+    const religiousness = ref(5)
+    const alcohol = ref(5)
+
+    const onSubmit = async (evt) => {
+      console.log(evt)
+      let res = await axios({
+        method: 'post',
+        url: 'https://randomthing2.free.beeceptor.com',
+        headers: {},
+        data: {
+          safety: safety.value,
+          price: price.value,
+          weather: weather.value,
+          english: english.value,
+          walkability: walkability.value,
+          cleanliness: cleanliness.value,
+          religiousness: religiousness.value,
+          alcohol: alcohol.value
+        }
+      })
+
+      //let data = res.data;
+      //console.log(data);
+      //evt.target.submit()
+    }
+
+    return {
+      safety, price, weather, english, walkability, cleanliness, religiousness, alcohol, onSubmit
+
+    };
+  },
+})
+</script>
---
+++ travel-planner/src/pages/IndexPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page>
+    <div class="row">
+      <div class="justify-center full-height full-width text-center">
+        <divzzz class="q-pa-md row items-start q-gutter-md">
+          <q-card class="my-card">
+            <q-card-section>
+              <q-slider
+                v-model="model"
+                marker-labels
+                :min="0"
+                :max="6"
+              />
+            </q-card-section>
+          </q-card>
+        </divzzz>
+      </div>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      slider: ref(50),
+      range: ref({
+        min: 10,
+        max: 30
+      }),
+
+      knob: ref(50),
+
+      time: ref(''),
+      date: ref('')
+    }
+  }
+}
+</script>
