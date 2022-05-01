0dacec04dcffb3123e2b743895fd1fa99857d247
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2022-05-01 05:11:02 +0200

    small app.py changes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import logging
+
 from flask import Flask
 from flask import request
 from flask import Response
 from flask_cors import cross_origin
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 from flask import jsonify
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/submit', methods=['POST', 'OPTIONS'])
 @cross_origin()
-def hello_world():  # put application's code here
+def get_rankings():  # put application's code here
     data = request.get_json()
     safety = data['safety']
     price = data['price']
@@ -21,14 +22,6 @@ def hello_world():  # put application's code here
     cleanliness = data['cleanliness']
     religiousness = data['religiousness']
     alcohol = data['alcohol']
-
-    df = pd.read_csv('data/full_data.csv')
-    df.fillna(df.mean(), inplace=True)
-
-    df['precipitation'] = 1 - df['precipitation']
-    df['windy days'] = 1 - df['windy days']
-    df['index_prices'] = 1 - df['index_prices']
-    df['index_safety'] = 1 - df['index_safety']
 
     df['score'] = df['eng'] * english ** 2 + df['top 5 distance'] * walkability ** 2 + df['temp'] * weather ** 2 + \
                   df['precipitation'] * weather ** 2 + df['sunshine'] * weather ** 2 + df['windy days'] * weather ** 2 + \
@@ -43,5 +36,11 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
+    df = pd.read_csv('data/full_data.csv')
+    df.fillna(df.mean(), inplace=True)
 
+    df['precipitation'] = 1 - df['precipitation']
+    df['windy days'] = 1 - df['windy days']
+    df['index_prices'] = 1 - df['index_prices']
+    df['index_safety'] = 1 - df['index_safety']
     app.run()
