1ba29b74025f390bdcb79bf8e07d14a55984e295
Jan Warchocki <j.warchocki@student.tudelft.nl>
2022-05-01 04:11:37 +0200

    Added setup for backend

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask import request
+from flask import Response
+from flask_cors import cross_origin
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/submit', methods=['POST', 'OPTIONS'])
+@cross_origin()
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    data = request.get_json()
+    print(data)
+
+    return Response(status=200)
 
 
 if __name__ == '__main__':
+
     app.run()
