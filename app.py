72c19a1d078e7e3ae284d6dae394af73b6e592c5
Jan Warchocki <j.warchocki@student.tudelft.nl>
2022-05-01 04:43:08 +0200

    Added basic recommender

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Response
 from flask_cors import cross_origin
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -12,9 +13,32 @@ app = Flask(__name__)
 @cross_origin()
 def hello_world():  # put application's code here
     data = request.get_json()
-    print(data)
+    safety = data['safety']
+    price = data['price']
+    weather = data['weather']
+    english = data['english']
+    walkability = data['walkability']
+    cleanliness = data['cleanliness']
+    religiousness = data['religiousness']
+    alcohol = data['alcohol']
 
-    return Response(status=200)
+    df = pd.read_csv('data/full_data.csv')
+    df.fillna(df.mean(), inplace=True)
+
+    df['precipitation'] = 1 - df['precipitation']
+    df['windy days'] = 1 - df['windy days']
+    df['index_prices'] = 1 - df['index_prices']
+
+    df['score'] = df['eng'] * english ** 2 + df['top 5 distance'] * walkability ** 2 + df['temp'] * weather ** 2 + \
+                  df['precipitation'] * weather ** 2 + df['sunshine'] * weather ** 2 + df['windy days'] * weather ** 2 + \
+                  df['index_alcohol'] * alcohol ** 2 + df['index_religion'] * religiousness ** 2 + \
+                  df['index_prices'] * price ** 2 + df['index_cleanliness'] * cleanliness ** 2 + df['index_safety'] * safety ** 2
+
+    df.sort_values('score', ascending=False, inplace=True)
+
+    best_countries = list(df['country'].iloc[:10].values)
+
+    return jsonify(best_countries)
 
 
 if __name__ == '__main__':
